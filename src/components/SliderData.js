150905008554e6fcbc7fd5a578e44a8cad895110
Ayman <aymaneelfounti@gmail.com>
2023-10-31 21:18:37 +0100

    Commit All Files

---
+++ src/components/SliderData.js
@@ -0,0 +1,22 @@
+export const SliderData = [
+    {
+        image:
+        'https://imgs.search.brave.com/Ev1xv0HhyOrzgVmZr5HQ8M9wV_hi6HTbcgJU7nDxdCg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuYWxsLWZyZWUt/ZG93bmxvYWQuY29t/L2ltYWdlcy9ncmFw/aGljbGFyZ2UvbmF0/dXJhbF9zY2VuZV9w/aWN0dXJlX2VsZWdh/bnRfY2xvc2V1cF9i/dXR0ZXJmbHlfcGVy/Y2hpbmdfcGV0YWxf/NjkzMTc4Mi5qcGc'
+      },
+      {
+        image:
+          'https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80'
+      },
+      {
+        image:
+          'https://imgs.search.brave.com/X7J7WwKNhG-TnF-cMOrTZ6bFiUrB7JDMMQ_lap_wiRQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTE1/OTY0NzM5L3Bob3Rv/L3ZpZXctb2YtbWlh/bWktYXQtc3Vuc2V0/LXNwZWNpYWwtcGhv/dG9ncmFwaGljLXBy/b2Nlc3NpbmcuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPXFB/MkRnS3JycFA5a1lK/NEJkalNDeGlVRnYy/LTFrckZRSWg2Nlht/NlRBRGs9'
+      },
+      {
+        image:
+          'https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80'
+      },
+      {
+        image:
+          'https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+      }
+];
